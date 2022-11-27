c4edcf2d1c8c383f0a4a9952dfb80a156bc9ab4e
shosei723 <85082346+shosei723@users.noreply.github.com>
2022-11-27 19:39:03 +0900

    Add files via upload

---
+++ omikuji/test1.js
@@ -0,0 +1,27 @@
+let name;
+let username;
+let userresult;
+
+username = prompt("お名前を教えて下さい。");
+if (username==""){
+  username = "名無しさん";
+}
+document.getElementById("name").innerHTML = username;
+
+let rand = Math.floor( Math.random() * 5);
+if (rand == 0) {
+  userresult = "大吉";
+}
+if (rand == 1) {
+  userresult = "中吉";
+}
+if (rand == 2) {
+  userresult = "小吉";
+}
+if (rand == 3) {
+  userresult = "吉";
+}
+if (rand == 4) {
+  userresult = "凶";
+}
+document.getElementById("result").innerHTML = userresult;
